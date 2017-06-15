8e86d06fc986404d696831077145afdff191966e
Perska <perska.weegee@gmail.com>
2017-06-15 19:36:47 +0300

    woww

--- site.js
+++ site.js
@@ -1,3 +1,11 @@
 function loadComic(index){
-	//there's no comics yet so I can be lazy and not add the code yet
+	var comics = JSON.parse(loadFile("/comic.json"));
+	alert(comics[0].name);
+}
+
+function loadFile(file){
+	var xml = new XMLHttpRequest();
+	xml.open("GET", file + "?t=" + (new Date()).getTime(), false);
+	xml.send();
+	return xml.responseText;
 }
